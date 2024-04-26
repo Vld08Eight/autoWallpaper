c69dd5445b230808f7e3152427696f151469d381
Vld08Eight <vld088mail@gmail.com>
2024-04-26 19:03:36 +0300

    get imgs matrix

--- main.py
+++ main.py
@@ -48,15 +48,17 @@ def hex2rgb(hexcode):
     return tuple(map(ord,hexcode[1:].decode('hex')))
 
 def reload_files():
+    files = glob.glob(dir)
+    print(len(files))
     for file in files:
-        perc = get_max_percent(get_colors(file))
-        print(file)
-        print(perc)
-        print(rgb2hex(int(perc[0][0]),int(perc[0][1]),int(perc[0][2])))
-        tohex = rgb2hex(int(perc[0][0]),int(perc[0][1]),int(perc[0][2]))
-        if (os.path.basename(file).split("_")[0] != tohex):
+        if (os.path.basename(file).split("_")[0][0] != "#"):
+            perc = get_max_percent(get_colors(file))
+        #print(file)
+        #print(perc)
+        #print(rgb2hex(int(perc[0][0]),int(perc[0][1]),int(perc[0][2])))
+            tohex = rgb2hex(int(perc[0][0]),int(perc[0][1]),int(perc[0][2]))
             os.rename(file, file.replace(os.path.basename(file), tohex+"_"+str(perc[1])+".jpg"))
-            print(file.replace(os.path.basename(file), tohex+"_"+str(perc[1])+".jpg"))
+            #print(file.replace(os.path.basename(file), tohex+"_"+str(perc[1])+".jpg"))
 
 def initialise(dir):
     files = glob.glob(dir)
@@ -96,6 +98,61 @@ def get_compared_images_with(color, arr, count):
     return outArr
 
 
-arr = initialise(dir)
-outArr = get_compared_images_with(arr[4][0], arr, 10)
+def get_arr_paths(arr):
+    files = glob.glob(dir)
+    newArr = []
+    for i in arr:
+        newArr.append(files[i])
+    return newArr
+
+def connect_rigth(img1, img2):
+    # Select the image files to be merged
+    img_file_1 = img1
+    img_file_2 = img2
+
+    # Open the image files
+    try:
+        im_1 = Image.open(img_file_1)
+    except:
+        im_1 = img1
+    try:
+        im_2 = Image.open(img_file_2)
+    except:
+        im_2 = img2
+    # Resize the images to the same size
+    #im_1 = im_1.resize((400, 400))
+    #im_2 = im_2.resize((400, 400))
+
+    # ИЗМЕНИТЬ ЦВЕТ С 250 на средний
+    new_image = Image.new('RGB', ((im_2.size[0] + im_1.size[0]), max(im_1.size[1], im_2.size[1])), (250, 250, 250))
+
+    # Paste the images onto the new image
+    new_image.paste(im_1, (0, 0))
+    new_image.paste(im_2, (im_1.size[0], 0))
+
+    # Save the merged image in the desired format
+    #new_image.save("some_image3.jpg", "JPEG")
+    return new_image
+
+def create_img_matrix(arr, x, y):
+  img_matrix = [ [None] * x for _ in range(y) ]
+  for i in range(y):
+    for j in range(x):
+      img = Image.open(arr[i*x+j])
+      img_matrix[i][j] = (arr[i*x+j], (img.size[0], img.size[1]))
+  return img_matrix
+
+reload_files()#init files in folder
+arr = initialise(dir)# get arr of all imgs in dir
+x = 8
+y = 6
+img_Matrix = d = [ [ ("",[0,0]) for j in range( x ) ] for i in range( y ) ]
+outArr = get_compared_images_with(arr[4][0], arr, x*y)# get arr of names imgs with similar color
 print(outArr)
+nArr = get_arr_paths(outArr)# img paths
+
+img_matrix = create_img_matrix(get_arr_paths(get_compared_images_with(arr[4][0], arr, x*y)), x, y)
+
+for i in range(y):
+  for j in range(x):
+    print(img_matrix[i][j])
